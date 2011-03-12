087726e4ae689e2ada693f35bcf5ba086fc22077
Simon Rigby <rigby@onarandombox.com>
2011-03-12 18:56:58 +0000

    Basic Commenting to test the IRC Bot again.

--- src/com/onarandombox/MultiVerseSignPortals/MultiVerseSignPortals.java
+++ src/com/onarandombox/MultiVerseSignPortals/MultiVerseSignPortals.java
@@ -20,21 +20,27 @@ public class MultiVerseSignPortals extends JavaPlugin {
 	public UpdateChecker updateCheck;
 	
 	public void onEnable() {
-		
-	    core = (MultiVerseCore) getServer().getPluginManager().getPlugin("MultiVerse-Core"); 
-		
-		if (core == null) {
-		    log.info(logPrefix + "MultiVerse-Core not found, will keep looking.");
-			getServer().getPluginManager().disablePlugin(this);
-			return;
-		}
-		
-		playerListener = new MVSPPlayerListener(this);
-		
+	    // Register the PLUGIN_ENABLE Event as we will need to keep an eye out for the Core Enabling if we don't find it initally.
+	    getServer().getPluginManager().registerEvent(Type.PLUGIN_ENABLE, playerListener, Priority.Normal, this);
+	    
+		// Try and grab the Core Plugin, if it doesn't exist it will return null.
+	    core = (MultiVerseCore) getServer().getPluginManager().getPlugin("MultiVerse-Core");
+
+	    // Test if the Core was found, if not we'll disable this plugin.
+        if (core == null) {
+            log.info(logPrefix + "MultiVerse-Core not found, will keep looking.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+	    
+        // Setup the Player Listener, we only need to listen out for PLAYER_MOVE Events.
+		playerListener = new MVSPPlayerListener(this);		
 		getServer().getPluginManager().registerEvent(Type.PLAYER_MOVE, playerListener, Priority.Normal, this);
 		
+		// Simple Log output to state the plugin is ENABLED
 		log.info(logPrefix + "- Version " + this.getDescription().getVersion() + " Enabled");
 		
+		// Setup the Update Checker, this will check every 30 minutes for an update to the plugin and output to the console.
 		updateCheck = new UpdateChecker(this.getDescription().getName(),this.getDescription().getVersion());
 	}
 	
