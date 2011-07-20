eb7f32bcf7df6e36ff1f045653dc2260696b0db0
Rigby <rigby@onarandombox.com>
2011-07-20 19:40:19 +0100

    Maven Setup

---
+++ src/main/java/com/onarandombox/MultiverseSignPortals/MVSPPluginListener.java
@@ -0,0 +1,22 @@
+package com.onarandombox.MultiverseSignPortals;
+
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.ServerListener;
+
+import com.onarandombox.MultiverseCore.MultiverseCore;
+
+public class MVSPPluginListener extends ServerListener {
+
+    MultiverseSignPortals plugin;
+    
+    public MVSPPluginListener(MultiverseSignPortals plugin){
+        this.plugin = plugin;
+    }
+    
+    public void onPluginEnable(PluginEnableEvent event){
+        if (event.getPlugin().getDescription().getName().equals("Multiverse-Core")) {
+            this.plugin.core = ((MultiverseCore) plugin.getServer().getPluginManager().getPlugin("Multiverse-Core"));
+            this.plugin.getServer().getPluginManager().enablePlugin(this.plugin);
+        }
+    }
+}
