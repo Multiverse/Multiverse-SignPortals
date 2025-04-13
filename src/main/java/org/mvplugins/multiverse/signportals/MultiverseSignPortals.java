e02af3f768ce9bb2ef222ec762dbf54ca0820f27
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-04-13 13:51:46 +0800

    Update to support core api changes

--- src/main/java/org/mvplugins/multiverse/signportals/MultiverseSignPortals.java
+++ src/main/java/org/mvplugins/multiverse/signportals/MultiverseSignPortals.java
@@ -9,81 +9,42 @@ package org.mvplugins.multiverse.signportals;
 
 import com.dumptruckman.minecraft.util.Logging;
 import org.mvplugins.multiverse.core.MultiverseCoreApi;
-import org.mvplugins.multiverse.core.MultiversePlugin;
 import org.mvplugins.multiverse.core.config.CoreConfig;
 import org.mvplugins.multiverse.core.inject.PluginServiceLocator;
-import org.mvplugins.multiverse.core.inject.PluginServiceLocatorFactory;
+import org.mvplugins.multiverse.core.module.MultiverseModule;
 import org.mvplugins.multiverse.core.utils.StringFormatter;
 import org.mvplugins.multiverse.external.jvnet.hk2.annotations.Service;
-import org.mvplugins.multiverse.external.vavr.control.Option;
-import org.mvplugins.multiverse.external.vavr.control.Try;
 import org.mvplugins.multiverse.signportals.listeners.SignPortalsListener;
 
 import java.util.logging.Level;
 
 @Service
-public class MultiverseSignPortals extends MultiversePlugin {
+public class MultiverseSignPortals extends MultiverseModule {
 
     private static final double TARGET_CORE_API_VERSION = 5.0;
 
-    private MultiverseCoreApi core;
-    private PluginServiceLocator serviceLocator;
-
     /** This fires before I get Enabled. */
+    @Override
     public void onLoad() {
         Logging.init(this);
         this.getDataFolder().mkdirs();
     }
 
+    @Override
     public void onEnable() {
         Logging.init(this);
 
-        this.core = MultiverseCoreApi.get();
-
-        initializeDependencyInjection();
-        registerEvents();
+        initializeDependencyInjection(new MultiverseSignPortalsPluginBinder(this));
+        registerEvents(SignPortalsListener.class);
         Logging.setDebugLevel(serviceLocator.getActiveService(CoreConfig.class).getGlobalDebug());
 
         Logging.log(true, Level.INFO, " Enabled - By %s", StringFormatter.joinAnd(getDescription().getAuthors()));
     }
 
+    @Override
     public void onDisable() {
-        // The Usual
         shutdownDependencyInjection();
         Logging.info("- Disabled");
-    }
-
-    private void initializeDependencyInjection() {
-        serviceLocator = PluginServiceLocatorFactory.get()
-                .registerPlugin(new MultiverseSignPortalsPluginBinder(this), core.getServiceLocator())
-                .flatMap(PluginServiceLocator::enable)
-                .getOrElseThrow(exception -> {
-                    Logging.severe("Failed to initialize dependency injection!");
-                    getServer().getPluginManager().disablePlugin(this);
-                    return new RuntimeException(exception);
-                });
-    }
-
-    private void shutdownDependencyInjection() {
-        Option.of(serviceLocator)
-                .peek(PluginServiceLocator::disable)
-                .peek(ignore -> serviceLocator = null);
-    }
-
-    /**
-     * Function to Register all the Events needed.
-     */
-    private void registerEvents() {
-        var pluginManager = getServer().getPluginManager();
-
-        Try.run(() -> serviceLocator.getAllServices(SignPortalsListener.class).forEach(
-                        listener -> {
-                            Logging.info(listener.toString());
-                            pluginManager.registerEvents(listener, this);
-                        }))
-                .onFailure(e -> {
-                    throw new RuntimeException("Failed to register listeners. Terminating...", e);
-                });
     }
 
     @Override
--- src/main/java/org/mvplugins/multiverse/signportals/MultiverseSignPortalsPluginBinder.java
+++ src/main/java/org/mvplugins/multiverse/signportals/MultiverseSignPortalsPluginBinder.java
@@ -1,10 +1,11 @@
 package org.mvplugins.multiverse.signportals;
 
 import org.mvplugins.multiverse.core.inject.binder.JavaPluginBinder;
+import org.mvplugins.multiverse.core.module.MultiverseModuleBinder;
 import org.mvplugins.multiverse.external.glassfish.hk2.utilities.binding.ScopedBindingBuilder;
 import org.mvplugins.multiverse.external.jetbrains.annotations.NotNull;
 
-public class MultiverseSignPortalsPluginBinder extends JavaPluginBinder<MultiverseSignPortals> {
+public class MultiverseSignPortalsPluginBinder extends MultiverseModuleBinder<MultiverseSignPortals> {
 
     protected MultiverseSignPortalsPluginBinder(@NotNull MultiverseSignPortals plugin) {
         super(plugin);
