5e101874a408d719fed08ea4f578fbe398739fe9
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2025-06-26 14:51:19 +0000

    Deploy javadoc for 5.0.1-pre

---
+++ javadoc/5.0.1-SNAPSHOT/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mvplugins.multiverse.signportals"},{"l":"org.mvplugins.multiverse.signportals.enums"},{"l":"org.mvplugins.multiverse.signportals.exceptions"},{"l":"org.mvplugins.multiverse.signportals.listeners"},{"l":"org.mvplugins.multiverse.signportals.utils"}];updateSearchResults();
