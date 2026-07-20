68a260a91d85f491b076b41d12fc9987c8c4f524
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2026-07-20 20:37:55 +0800

    Fix compatibility with nether portals plugin

--- src/main/java/org/mvplugins/multiverse/signportals/MultiverseSignPortals.java
+++ src/main/java/org/mvplugins/multiverse/signportals/MultiverseSignPortals.java
@@ -8,12 +8,19 @@
 package org.mvplugins.multiverse.signportals;
 
 import com.dumptruckman.minecraft.util.Logging;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.mvplugins.multiverse.core.config.CoreConfig;
 import org.mvplugins.multiverse.core.module.MultiverseModule;
+import org.mvplugins.multiverse.core.utils.ReflectHelper;
 import org.mvplugins.multiverse.core.utils.StringFormatter;
 import org.jvnet.hk2.annotations.Service;
+import org.mvplugins.multiverse.external.jakarta.inject.Inject;
+import org.mvplugins.multiverse.external.jakarta.inject.Provider;
+import org.mvplugins.multiverse.external.vavr.control.Try;
+import org.mvplugins.multiverse.netherportals.MultiverseNetherPortalsApi;
 import org.mvplugins.multiverse.signportals.listeners.SignPortalsListener;
+import org.mvplugins.multiverse.signportals.utils.PortalDetector;
 
 import java.util.logging.Logger;
 
@@ -21,6 +28,9 @@ import java.util.logging.Logger;
 public class MultiverseSignPortals extends MultiverseModule {
 
     private static final double TARGET_CORE_API_VERSION = 5.0;
+
+    @Inject
+    private Provider<PortalDetector> portalDetectorProvider;
 
     /** This fires before I get Enabled. */
     @Override
@@ -34,9 +44,25 @@ public class MultiverseSignPortals extends MultiverseModule {
         initializeDependencyInjection(new MultiverseSignPortalsPluginBinder(this));
         Logging.setDebugLevel(serviceLocator.getActiveService(CoreConfig.class).getGlobalDebug());
         registerDynamicListeners(SignPortalsListener.class);
+        registerNetherPortalsHandleCheck();
 
         Logging.config("Version %s (API v%s) Enabled - By %s",
                 this.getDescription().getVersion(), getVersionAsNumber(), StringFormatter.joinAnd(this.getDescription().getAuthors()));
+    }
+
+    private void registerNetherPortalsHandleCheck() {
+        if (!ReflectHelper.hasClass("org.mvplugins.multiverse.netherportals.MultiverseNetherPortalsApi")) {
+            return;
+        }
+        MultiverseNetherPortalsApi.whenLoaded(api ->
+                api.getCustomPortalsHandler().registerHandleCheck(((entity, portalLocation) -> {
+                    if (!(entity instanceof Player player)) {
+                        return false;
+                    }
+                    Logging.warning("Checking if player %s can use a portal at %s", player.getName(), portalLocation);
+                    return Try.of(() -> portalDetectorProvider.get().getNotchPortalDestination(player, portalLocation) != null)
+                            .getOrElse(false);
+                })));
     }
 
     @Override
